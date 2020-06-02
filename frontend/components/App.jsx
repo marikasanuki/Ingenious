bc8bcf49cd59242ba28a6b49fcdf95d0274983b0
marikasanuki <mari.kasanuki@gmail.com>
2020-06-02 10:34:12 -0400

    application, users, and sessions controllers updates

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => (
     <div>
-        <h1>hi this is Ingenious</h1>
+        <h1>Hi, this is the App.jsx file</h1>
     </div>
 );
 
--- frontend/ingenious.jsx
+++ frontend/ingenious.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { configureStore } from './store/store.js'
+import configureStore from './store/store.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root')
