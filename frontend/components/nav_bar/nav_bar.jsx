3664d67f4c755465d7b16d7c1ca594cde5ed528e
marikasanuki <mari.kasanuki@gmail.com>
2020-06-06 16:56:36 -0400

    nav bar icons added

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 class NavBar extends React.Component {
     render() {
         return (
             <div className='nav-bar'>
                 <ul className='nav-bar-list'>
-                    <li><a className='nav-bar-item' href="https://github.com/marikasanuki" target="blank" >FEATURED</a></li>   
+                    <li><a className='nav-bar-item' href="https://github.com/marikasanuki" target="blank" >GITHUB</a></li>   
                     <li className='nav-bar-item' >&nbsp;|&nbsp;</li>
-                    <li><Link className='nav-bar-item' to={`/tracks`}> CHARTS </Link></li>
+                    <li><Link className='nav-bar-item' to={`/tracks`}>CHARTS</Link></li>
                     <li className='nav-bar-item' >&nbsp;|&nbsp;</li>                    
                     <li><a className='nav-bar-item' href="https://www.linkedin.com/in/marikasanuki/" target="blank" >LINKEDIN</a></li>                    
                     <li className='nav-bar-item' >&nbsp;|&nbsp;</li>
                     <li><a className='nav-bar-item' href="https://teespring.com/stores/genius-merch" target="blank" >SHOP</a></li>
                     <li className='nav-bar-item' >&nbsp;|&nbsp;</li>
-                    <li><a className='nav-bar-item' href="https://github.com/marikasanuki" target="blank" >ICON&nbsp;LINKS</a></li>
+                    <li>
+                        <a className='nav-bar-item' href="https://github.com/marikasanuki" target="blank" >
+                            <FontAwesomeIcon className='fa-icon' icon={faGithub} />
+                        </a>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a className='nav-bar-item' href="https://www.linkedin.com/in/marikasanuki/" target="blank" >
+                            <FontAwesomeIcon className='fa-icon' icon={faLinkedinIn} />
+                        </a>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a className='nav-bar-item' href="https://twitter.com/marikasanuki?lang=en" target="blank" >
+                            <FontAwesomeIcon className='fa-icon' icon={faTwitter} />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </a>
+                    </li>
                 </ul>                 
             </div>
         )
